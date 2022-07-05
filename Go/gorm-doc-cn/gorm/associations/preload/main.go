7354d955522fd4236318c54b58eef58c6a74078d
Eminem-x <1773997586@qq.com>
2022-07-05 10:34:11 +0800

    Update:新增时间戳转化技巧

--- Go/gorm-doc-cn/gorm/associations/preload/main.go
+++ Go/gorm-doc-cn/gorm/associations/preload/main.go
@@ -23,5 +23,16 @@ func main() {
     db.Create(&product)
     fmt.Println(product.CreatedAt)
     time.Sleep(100000000)
+
+    // 获取 duration
     fmt.Println(uint(time.Since(product.CreatedAt).Seconds()))
+
+    // 获取 format 时间
+    timestamp := product.UpdatedAt.Unix()
+    tm := time.Unix(timestamp, 0)
+    fmt.Println(tm.Format("2006-01-02 15:04:05")
+
+    // 时间转为时间戳
+    timestamp = strconv.FormatInt(product.UpdatedAt.Unix(), 10)
+    fmt.Println(timestamp)
 }
