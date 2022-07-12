82a13f2358baecb7319f66b1f574dbb7773e9314
Eminem-x <1773997586@qq.com>
2022-07-12 15:56:26 +0800

    Update main.go

--- Go/gorm-doc-cn/gorm/associations/preload/main.go
+++ Go/gorm-doc-cn/gorm/associations/preload/main.go
@@ -4,35 +4,36 @@ import (
     "fmt"
     "gorm.io/gorm"
     "gorm/config"
+    "strconv"
     "time"
 )
 
 var db *gorm.DB
 
 type Product struct {
-    gorm.Model // 自定义主键 ID
-    Code       string
-    Price      uint
+	gorm.Model // 自定义主键 ID
+	Code       string
+	Price      uint
 }
 
 // Get timestamp
 func main() {
-    db = config.InitDB()
-    var product Product
-    product.ID = 251
-    db.Create(&product)
-    fmt.Println(product.CreatedAt)
-    time.Sleep(100000000)
+	db = config.InitDB()
+	var product Product
+	product.ID = 251
+	db.Create(&product)
+	fmt.Println(product.CreatedAt)
+	time.Sleep(100000000)
 
-    // 获取 duration
-    fmt.Println(uint(time.Since(product.CreatedAt).Seconds()))
+	// 获取 duration
+	fmt.Println(uint(time.Since(product.CreatedAt).Seconds()))
 
-    // 获取 format 时间
-    timestamp := product.UpdatedAt.Unix()
-    tm := time.Unix(timestamp, 0)
-    fmt.Println(tm.Format("2006-01-02 15:04:05")
+	// 获取 format 时间
+	timestamp := product.UpdatedAt.Unix()
+	tm := time.Unix(timestamp, 0)
+	fmt.Println(tm.Format("2006-01-02 15:04:05"))
 
-    // 时间转为时间戳
-    timestamp = strconv.FormatInt(product.UpdatedAt.Unix(), 10)
-    fmt.Println(timestamp)
+	// 时间转为时间戳
+	t := strconv.FormatInt(product.UpdatedAt.Unix(), 10)
+	fmt.Println(t)
 }
