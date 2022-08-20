a91c680334657dddda49064de68d377f7b27e694
Eminem-x <1773997586@qq.com>
2022-08-20 22:32:40 +0800

    create:package-go-flags

---
+++ Go/package/go-flag/go.mod
@@ -0,0 +1,8 @@
+module go-flag
+
+go 1.18
+
+require (
+	github.com/jessevdk/go-flags v1.5.0 // indirect
+	golang.org/x/sys v0.0.0-20220818161305-2296e01440c6 // indirect
+)
---
+++ Go/package/go-flag/main.go
@@ -0,0 +1,56 @@
+// @Title go-flags
+// @Description learn go package: go-flags
+// @Author Yuanhao
+// @Update 2022-08-20
+package main
+
+import (
+	"fmt"
+
+	"github.com/jessevdk/go-flags"
+)
+
+// 博客 https://darjun.github.io/2020/01/10/godailylib/go-flags/
+// 官方 https://github.com/jessevdk/go-flags
+type Option struct {
+	IntFlag        int            `short:"i" long:"int" description:"int flag value"`
+	IntSlice       []int          `long:"intslice" description:"int slice flag value"`
+	BoolFlag       bool           `long:"bool" description:"bool flag value"`
+	BoolSlice      []bool         `long:"boolslice" description:"bool slice flag value"`
+	FloatFlag      float64        `long:"float" description:"float64 flag value"`
+	FloatSlice     []float64      `long:"floatslice" description:"float64 slice flag value"`
+	StringFlag     string         `short:"s" long:"string" description:"string flag value"`
+	StringSlice    []string       `long:"strslice" description:"string slice flag value"`
+	PtrStringSlice []*string      `long:"pstrslice" description:"slice of pointer of string flag value"`
+	Call           func(string)   `long:"call" description:"callback"`
+	IntMap         map[string]int `long:"intmap" description:"A map from string to int"`
+}
+
+func main() {
+	// go build -o flags main.go
+	// ./flags --pstrslice test1 --pstrslice test2
+	var opt Option
+	opt.Call = func(value string) {
+		fmt.Println("in callback: ", value)
+	}
+
+	_, err := flags.Parse(&opt)
+	if err != nil {
+		fmt.Println("Parse error:", err)
+		return
+	}
+
+	fmt.Printf("int flag: %v\n", opt.IntFlag)
+	fmt.Printf("int slice flag: %v\n", opt.IntSlice)
+	fmt.Printf("bool flag: %v\n", opt.BoolFlag)
+	fmt.Printf("bool slice flag: %v\n", opt.BoolSlice)
+	fmt.Printf("float flag: %v\n", opt.FloatFlag)
+	fmt.Printf("float slice flag: %v\n", opt.FloatSlice)
+	fmt.Printf("string flag: %v\n", opt.StringFlag)
+	fmt.Printf("string slice flag: %v\n", opt.StringSlice)
+	fmt.Println("slice of pointer of string flag: ")
+	for i := 0; i < len(opt.PtrStringSlice); i++ {
+		fmt.Printf("\t%d: %v\n", i, *opt.PtrStringSlice[i])
+	}
+	fmt.Printf("int map: %v\n", opt.IntMap)
+}
